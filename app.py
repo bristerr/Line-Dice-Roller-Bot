11fb749f4a6df5a334b450d888182255ef5c54f3
bristerr <bristerr@bu.edu>
2018-03-30 00:32:33 -0400

    Update app.py

--- app.py
+++ app.py
@@ -296,7 +296,7 @@ def handle_message(event):
     print("event.reply_token:", event.reply_token)
     print("event.message.text:", event.message.text)
     if event.message.text == "eyny":
-        content = eyny_movie()
+        content = event.message.text
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=content))

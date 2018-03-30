5653a640cbf729a0f288ab3b23134648763b6cd4
bristerr <bristerr@bu.edu>
2018-03-30 03:16:23 -0400

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ def DiceRoller(sms):
 def handle_message(event):
     print("event.reply_token:", event.reply_token)
     print("event.message.text:", event.message.text)
-    if "Slavonic" in event.message.text:
+    if ".d" in event.message.text:
         sms = event.message.text
         content = DiceRoller(sms)
         line_bot_api.reply_message(

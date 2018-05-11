4b65afafbc7a69792ecb98951d1063eddf1eb092
bristerr <bristerr@bu.edu>
2018-05-11 14:53:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def Year():
     today = datetime.date.today()
     end_date = today- start
     year=end_date.days
-    content = 1561 + year
+    content = 1572 + year
     return content
 
 @handler.add(MessageEvent, message=TextMessage)

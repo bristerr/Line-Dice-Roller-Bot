8eb7eebb7c438de695865aa3ddb2be71c55d29b0
bristerr <bristerr@bu.edu>
2019-02-02 12:59:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -168,7 +168,7 @@ def Senate():
 
 def Year():
     import datetime 
-    start = datetime.date(2019, 2, 1)
+    start = datetime.date(2019, 2, 2)
     today = datetime.date.today()
     end_date = today- start
     year=end_date.days

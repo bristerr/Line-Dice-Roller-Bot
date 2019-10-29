5a5f6cc574d27a64cfccaa14f09ca82e3128eeba
Charles <bristerr@bu.edu>
2019-10-29 13:51:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -239,7 +239,7 @@ def handle_message(event):
             TextSendMessage(text=content))
         return 0
     
-    if "What's the year" in event.message.text:
+    if "the year?" in event.message.text:
         content = Year()
         line_bot_api.reply_message(
             event.reply_token,

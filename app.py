692b65af1e6a938f66e49c25ad442f4c2ff4560e
Charles <bristerr@bu.edu>
2019-11-26 18:00:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -273,6 +273,13 @@ def handle_message(event):
             TextSendMessage(text=content))
         return 0
     
+    if "What\'s the year" in event.message.text:
+        content = Year()
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=content))
+        return 0
+    
     if ".birth" in event.message.text:
         content = BRoller()
         line_bot_api.reply_message(

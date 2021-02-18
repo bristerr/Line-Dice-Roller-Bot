9745740cb6d24bc4977197e15b2b239ec53a177e
Charles <bristerr@bu.edu>
2021-02-17 23:01:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -176,11 +176,11 @@ def Open():
 
 def Year():
     import datetime 
-    start = datetime.date(2019, 12, 9)
+    start = datetime.date(2021, 1, 28)
     today = datetime.date.today()
     end_date = today- start
     year=end_date.days
-    content = 1380 + year
+    content = 1700
     #content = 'The year is paused at 1415'
     return content
 

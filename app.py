06f6c9150b48fa06c0f0d0b3e87d1e8dc9a4533f
Charles <bristerr@bu.edu>
2021-02-21 22:40:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import re
 import random
 import configparser
 from bs4 import BeautifulSoup
-from flask import Flask, request, abort
+from flask import Flask, request
 from imgurpython import ImgurClient
 from random import randint
 from linebot import (
